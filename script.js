a81e8855b45382b8fd76f4dcf0e53799ff710765
Amirakram02 <akramamir392@gmail.com>
2026-08-12 19:08:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ document.getElementById('quoteForm').addEventListener('submit',function(e){
 e.preventDefault();
 const d=new FormData(this);
 const message=`Hi Kajang Autogate, I would like to get a quotation.%0A%0AName: ${d.get('name')}%0APhone: ${d.get('phone')}%0AService: ${d.get('service')}%0ALocation: ${d.get('location')}%0AMessage: ${d.get('message')}`;
-window.open('https://wa.me/60123456789?text='+message,'_blank');
+window.open('https://wa.me/60189781540?text='+message,'_blank');
 });
